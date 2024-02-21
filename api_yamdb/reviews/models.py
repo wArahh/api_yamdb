13207fa4113de25b53b2ab693d86edaf81e4c239
Nikita <nikitkamakarenko@gmail.com>
2024-02-21 19:52:13 +0300

    base+reviews+comments

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+User = get_user_model()
+
+class Reviews(models.Model):
+    text = models.TextField(verbose_name='Текст', max_length=256)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='review',
+        verbose_name='Автор',
+    )
+    score = models.IntegerField(verbose_name='Оценка', validators=[MinValueValidator(1), MaxValueValidator(10)])
+    pub_date = models.DateField(verbose_name='Дата публикации', auto_now_add=True)
+
+
+
+class Comments(models.Model):
+    text = models.TextField(verbose_name='Текст', max_length=256)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comment',
+        verbose_name='Автор',
+    )
+    pub_date = models.DateField(verbose_name='Дата публикации', auto_now_add=True)
+
+
+class Title(models.Model):
+    name = models.CharField(verbose_name='Имя произведения', max_length=20)
+    # year =
+    rating = models.FloatField(verbose_name='Рейтинг', validators=[MinValueValidator(1), MaxValueValidator(10)])
+    description = models.TextField(verbose_name='Описание', max_length=256)
+    # genre =
+    # category =
